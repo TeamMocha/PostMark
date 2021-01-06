a6e8bdf46da901044ed7712e26a8211464c19ba7
Jeremy Penning <4085868+pixeljava@users.noreply.github.com>
2021-01-06 14:16:48 -0800

    Adding this so we can start writing tests.

--- sample/template.md
+++ sample/template.md
@@ -1,94 +1,42 @@
-Item: [object Object],[object Object],[object Object],[object Object],[object Object]
-Response URL: [object Object]
-Response URL: [object Object]
-Output: ## Another Collection
-**Description**:
-[Postman Collection JSON](<./assets/Another.postman_collection.json>)
+## Pokemon Test
+
+**Description**: This is a sample collection./n
+[Postman Collection JSON](<./assets/Really Good.postman_collection.json>)
 ## Route(s)
-**Description**:
-**Test URL**: [https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug/52fec04a-a6af-4849-a85d-ccf481ff73a0](https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug/52fec04a-a6af-4849-a85d-ccf481ff73a0)
-## Request
+**Description**: This call will get one Pokemon encounter method (Surf).
 
-#### **Method**: DELETE
->- Host: https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com
->- Path: /mug/52fec04a-a6af-4849-a85d-ccf481ff73a0
+**Test URL**: [https://pokeapi.co/api/v2/pokemon/ditto](https://pokeapi.co/api/v2/pokemon/ditto)
+
+### Request
+
+#### **Authorization Type**: bearer
+>- Key: token
+>- Value: (removed)
+>- Type: string
+
+
+#### **Method**: GET
+
+>- Host: https://pokeapi.co  
+>- Path: /api/v2/pokemon/ditto  
 #### **Query Parameter(s)**
 
 
 
-
-**Description**:
-**Test URL**: [https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug](https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug)
-## Request
-
-#### **Method**: POST
->- Host: https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com
->- Path: /mug
-#### **Query Parameter(s)**
-
-
+### Response(s)
 
 ---
-### **Status Code**: 500 ()
-#### POST /mug - 500 Failed
-**Test URL**: [https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug](https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug)
-#### **Method**: POST
-
->- Host: https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com
->- Path: /mug
-#### **Query Parameter(s)**
-
-
-
-#### **Body**
-```null
-
-```
-
-**Description**:
-**Test URL**: [https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug/96a4da43-407c-4c25-99a3-b60e9492c5f0](https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug/96a4da43-407c-4c25-99a3-b60e9492c5f0)
-## Request
-
-#### **Method**: PUT
->- Host: https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com
->- Path: /mug/96a4da43-407c-4c25-99a3-b60e9492c5f0
-#### **Query Parameter(s)**
-
-
-
-
-**Description**:
-**Test URL**: [https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug/](https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug/)
-## Request
-
+### **Status Code**: 404 (Not Found)
+#### GET /v2/encounter-method/685/?lang=en
+**Test URL**: [https://pokeapi.co/api/v2/encounter-method/685/?lang=en](https://pokeapi.co/api/v2/encounter-method/685/?lang=en)
 #### **Method**: GET
->- Host: https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com
->- Path: /mug/
+  
+>- Host: https://pokeapi.co  
+>- Path: /api/v2/encounter-method/685/  
 #### **Query Parameter(s)**
-
-
-
-
-**Description**: Get a _single_ mug
-**Test URL**: [https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug/{id}](https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug/{id})
-## Request
-
-#### **Method**: GET
->- Host: https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com
->- Path: /mug/{id}
-#### **Query Parameter(s)**
-
-
-
----
-### **Status Code**: 200 (OK)
-#### GET /mug/{id} - Sample Mug 1
-**Test URL**: [https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug/52fec04a-a6af-4849-a85d-ccf481ff73a0](https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug/52fec04a-a6af-4849-a85d-ccf481ff73a0)
-#### **Method**: GET
-
->- Host: https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com
->- Path: /mug/52fec04a-a6af-4849-a85d-ccf481ff73a0
-#### **Query Parameter(s)**
+##### Parameter (Response language)
+>- key: lang  
+>- value: en  
 
 
 >- Key: Content-Type
@@ -96,5 +44,78 @@ Output: ## Another Collection
 
 #### **Body**
 ```plain
-[{"capacity":240,"id":"52fec04a-a6af-4849-a85d-ccf481ff73a0","color":["Slate Grey","Blorange","blellow"],"droppable":true}]
+Not Found
+```
+---
+### **Status Code**: 200 (OK)
+#### GET /v2/encounter-method/5/?lang=en
+**Test URL**: [https://pokeapi.co/api/v2/encounter-method/5/?lang=en](https://pokeapi.co/api/v2/encounter-method/5/?lang=en)
+#### **Method**: GET
+  
+>- Host: https://pokeapi.co  
+>- Path: /api/v2/encounter-method/5/  
+#### **Query Parameter(s)**
+##### Parameter (Response language)
+>- key: lang  
+>- value: en  
+
+
+>- Key: Content-Type
+>- Value: application/json; charset=utf-8
+
+#### **Body**
+```json
+{
+    "id": 5,
+    "name": "surf",
+    "names": [
+        {
+            "language": {
+                "name": "de",
+                "url": "https://pokeapi.co/api/v2/language/6/"
+            },
+            "name": "Surfen"
+        },
+        {
+            "language": {
+                "name": "en",
+                "url": "https://pokeapi.co/api/v2/language/9/"
+            },
+            "name": "Surfing"
+        }
+    ],
+    "order": 14
+}
+```
+---
+### **Status Code**: 400 (Bad Request)
+#### GET /v2/encounter-method/(%)/?lang=en
+**Test URL**: [https://pokeapi.co/api/v2/encounter-method/(%)/?lang=en](https://pokeapi.co/api/v2/encounter-method/(%)/?lang=en)
+#### **Method**: GET
+  
+>- Host: https://pokeapi.co  
+>- Path: /api/v2/encounter-method/(%)/  
+#### **Query Parameter(s)**
+##### Parameter (Response language)
+>- key: lang  
+>- value: en  
+
+
+>- Key: Content-Type
+>- Value: text/html
+
+#### **Body**
+```html
+<html>
+    <head>
+        <title>400 Bad Request</title>
+    </head>
+    <body>
+        <center>
+            <h1>400 Bad Request</h1>
+        </center>
+        <hr>
+        <center>cloudflare</center>
+    </body>
+</html>
 ```
--- sample/test.js
+++ sample/test.js
@@ -1,23 +1,22 @@
 'use strict';
 
-let filepath = './assets/Another.postman_collection.json';
-
-let inputJSON = require(filepath);
-
+/////////////////////////////////////////////////////
+// Authentication Main Template (Request Template) //
+/////////////////////////////////////////////////////
+// Expecting (Request): input.item[i].request.auth
 const authTpl = (requestAuthObj) => {
-
   if(!requestAuthObj){
-    return;
+    return '';
   }
   
-  let authTemplate = "";
+  let authTemplate = '';
   let authTypeStr = requestAuthObj.type;
   authTemplate += `#### **Authorization Type**: ${authTypeStr}
 `;
   let authTypeArr = requestAuthObj[authTypeStr];
   for(let i = 0; i < authTypeArr.length; i++) {
     const authKeyStr = `>- Key: ${authTypeArr[i].key}`;
-    const authValueStr = `>- Value: ${authTypeArr[i].value}`;   
+    const authValueStr = `>- Value: [Hidden]`;   
     const authTypeTypeStr = `>- Type: ${authTypeArr[i].type}`;
     authTemplate += 
 `${authKeyStr}
@@ -28,31 +27,43 @@ ${authTypeTypeStr}
   return authTemplate;
 };
 
-const urlHostNameStr = (urlHostArray) => { // Expecting thisItem.request.url.host OR thisResponse.originalRequest.url.host
-  let hostName = "";
+///////////////////////////////////////////
+// URL Host Name Template (Url Template) //
+///////////////////////////////////////////
+// Expecting (Url): input.item[i].request.url.host OR input.item[i].response[i].originalRequest.url.host
+const urlHostNameStr = (urlHostArray) => {
+  let hostName = '';
   for (let i = 0; i < urlHostArray.length; i++) {
     if (i === urlHostArray.length - 1) {
-      hostName += `${urlHostArray[i]}`
+      hostName += `${urlHostArray[i]}`;
     } else {
-      hostName += `${urlHostArray[i]}.`
+      hostName += `${urlHostArray[i]}.`;
     }
   }
   return hostName; // Just a string representing the url host
 };
 
-const urlHostPathStr = (urlPathArray) => { // Expecting thisItem.request.url.path OR thisResponse.originalRequest.url.path
-  let hostPath = "";
+///////////////////////////////////////////
+// URL Host Path Template (Url Template) //
+///////////////////////////////////////////
+// Expecting (Url): input.item[i].request.url.path OR input.item[i].response[i].originalRequest.url.path
+const urlHostPathStr = (urlPathArray) => {
+  let hostPath = '';
   for (let i = 0; i < urlPathArray.length; i++) {
-    hostPath += `/${urlPathArray[i]}`
+    hostPath += `/${urlPathArray[i]}`;
   }
   return hostPath; // Just a string representing the url path
 };
 
-const urlQueryParamsTpl = (urlQueryArray) => { // Expecting thisItem.request.url.query OR thisResponse.originalRequest.url.query
+///////////////////////////////////////////////////
+// URL Query Parameters Template (Url Template)  //
+///////////////////////////////////////////////////
+// Expecting (Url): input.item[i].request.url.query OR input.item[i].response[i].originalRequest.url.query
+const urlQueryParamsTpl = (urlQueryArray) => {
   if(!urlQueryArray){
-    return;
+    return '';
   }
-  let hostParams = "";
+  let hostParams = '';
   for (let i = 0; i < urlQueryArray.length; i++) {
     hostParams +=
 `##### Parameter (${urlQueryArray[i].description})
@@ -60,13 +71,15 @@ const urlQueryParamsTpl = (urlQueryArray) => { // Expecting thisItem.request.url
 >- value: ${urlQueryArray[i].value}  
 `;
   }
-
   return hostParams; // A template for all query parameters 
 };
 
-const urlTpl = (urlObj) => { // Expecting thisItem.request.url OR thisResponse.originalRequest.url
-  // console.log(urlObj)
-
+//////////////////////////////////////////////////////
+// URL Main Template (Request & Response Templates) //
+//////////////////////////////////////////////////////
+// Expecting (Request): input.item[i].request.url
+// Expecting (Response): input.item[i].response[i].orginalRequest.url
+const urlTpl = (urlObj) => {
   var queryString = '';
   if (urlObj.query) {
     queryString = urlQueryParamsTpl(urlObj.query);
@@ -78,42 +91,54 @@ const urlTpl = (urlObj) => { // Expecting thisItem.request.url OR thisResponse.o
 #### **Query Parameter(s)**
 ${queryString}
 `;
-
   return urlTemplate;
 };
 
-const requestTpl = (requestObj) => { // Expecting thisItem.request OR thisResponse.originalRequest
+////////////////////////////////////////////
+// Request Main Template (Route Template) //
+////////////////////////////////////////////
+// Expecting (Route): input.item[i].request or input.item[i].response[i].originalRequest
+const requestTpl = (requestObj) => {
   // console.log('++++++++++', requestObj);
   if(!requestObj.description){
-    requestObj.description = "";
+    requestObj.description = '';
   }
 
-  var authString = "";
+  var authString = '';
   if (requestObj.auth) {
     authString = authTpl(requestObj.auth);
   }
 
   let requestTemplate =
 `**Description**: ${requestObj.description}
+
 **Test URL**: [${requestObj.url.raw}](${requestObj.url.raw})
-## Request
+
+### Request
+
 ${authString}
+
 #### **Method**: ${requestObj.method}
+
 ${urlTpl(requestObj.url)}
 `;
 
   return requestTemplate;
 };
 
-const headerTpl = (headerArr) => { //We are expecting input.item[0].response[0].header
-  let headerTemplate = "";
+//////////////////////////////////////////////
+// Header Main Template (Response Template) //
+//////////////////////////////////////////////
+// Expecting (Response): input.item[i].response[i].header
+const headerTpl = (headerArr) => {
+  let headerTemplate = '';
   
   if (headerArr === null) {
     return headerTemplate;
   }
   
   for(let i = 0; i < headerArr.length; i++){
-    if(headerArr[i].key === "Content-Type") {
+    if(headerArr[i].key === 'Content-Type') {
       const headerKeyStr = `>- Key: ${headerArr[i].key}`;
       const headerValueStr = `>- Value: ${headerArr[i].value}`;
       headerTemplate += 
@@ -125,8 +150,12 @@ ${headerValueStr}
   return headerTemplate;
 };
 
-const responseTpl = (responseArr) => { // Expecting thisItem.response 
-  let responseTemplate = "";
+/////////////////////////////////////////////
+// Response Main Template (Route Template) //
+/////////////////////////////////////////////
+// Expecting (Route): input.item[i].response
+const responseTpl = (responseArr) => {
+  let responseTemplate = '';
   for(let i = 0; i < responseArr.length; i++) {
 
     var statusString = '';
@@ -154,8 +183,12 @@ ${responseArr[i].body}
   return responseTemplate;
 };
 
+///////////////////////////////////////////////
+// Route Main Template (Everything Template) //
+///////////////////////////////////////////////
+// Expecting (routes): input.item
 const routeTpl = (routeArr) => {
-  let routeTemplate = "";
+  let routeTemplate = '';
   // console.log(routeArr);
   routeTemplate += `## Route(s)
 `;
@@ -167,25 +200,51 @@ const routeTpl = (routeArr) => {
     let responseArr = routeArr[i].response;
     if(responseArr) {
       routeTemplate +=
-`${responseTpl(responseArr)}
-`
+`### Response(s)
+
+${responseTpl(responseArr)}
+`;
     }
   }
 
   return routeTemplate;
 };
 
-var descriptionString = "";
-if(inputJSON.info.description) {
-  descriptionString = inputJSON.info.description;
-}
+/////////////////////////
+// Everything Template //
+/////////////////////////
+// Expecting (): input
+const everythingTpl = (input) => {
+  // Start our blank everythingTemplate
+  let everythingTemplate = '';
+  // Add the collection name
+  everythingTemplate += `## ${input.info.name}`;
+  // (Add a space between lines)
+  everythingTemplate += `
+  `;
+  // Add the collection description (if one was given)
+  if (input.info.description) { everythingTemplate += `**Description**: ${input.info.description}`; }
+  // (Add a space between lines)
+  everythingTemplate += `
+  `;
+  // Add a link to download the collection
+  everythingTemplate += `[Postman Collection JSON](<${filepath}>)`;
+  // (Add a space between lines)
+  everythingTemplate += `
+  `;
+  // Add the collection routes
+  everythingTemplate += `${routeTpl(input.item)}`;
+  return everythingTemplate;
+};
 
-// console.log("Item: " + input.item);
-let output = 
-`## ${inputJSON.info.name}
-**Description**: ${descriptionString}
-[Postman Collection JSON](<${filepath}>)
-${routeTpl(inputJSON.item)}
-`;
+// Bringing in the Postman Collection (Move this to TOF before committing!)
+// Eventually we will merge this code with our working 'fs' and 'prompt' code
+let filepath = './assets/Really Bad.postman_collection.json';
+let input = require(filepath); // JSON.parse(event.body); // filename
 
-console.log("Output: " + output);
+// Let out output variable kick off all of the templating functions.
+let output = everythingTpl(input);
+
+// Finally, return everything!
+console.log(output);
+
