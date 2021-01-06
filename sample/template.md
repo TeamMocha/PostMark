8f2d73082954c016b90ab3be8424e823056e88be
Sowmya Billakanti <sowmya.billakanti8@gmail.com>
2021-01-06 00:44:56 -0800

    Make test file generate a proper output

---
+++ sample/template.md
@@ -0,0 +1,100 @@
+Item: [object Object],[object Object],[object Object],[object Object],[object Object]
+Response URL: [object Object]
+Response URL: [object Object]
+Output: ## Another Collection
+**Description**:
+[Postman Collection JSON](<./assets/Another.postman_collection.json>)
+## Route(s)
+**Description**:
+**Test URL**: [https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug/52fec04a-a6af-4849-a85d-ccf481ff73a0](https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug/52fec04a-a6af-4849-a85d-ccf481ff73a0)
+## Request
+
+#### **Method**: DELETE
+>- Host: https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com
+>- Path: /mug/52fec04a-a6af-4849-a85d-ccf481ff73a0
+#### **Query Parameter(s)**
+
+
+
+
+**Description**:
+**Test URL**: [https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug](https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug)
+## Request
+
+#### **Method**: POST
+>- Host: https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com
+>- Path: /mug
+#### **Query Parameter(s)**
+
+
+
+---
+### **Status Code**: 500 ()
+#### POST /mug - 500 Failed
+**Test URL**: [https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug](https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug)
+#### **Method**: POST
+
+>- Host: https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com
+>- Path: /mug
+#### **Query Parameter(s)**
+
+
+
+#### **Body**
+```null
+
+```
+
+**Description**:
+**Test URL**: [https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug/96a4da43-407c-4c25-99a3-b60e9492c5f0](https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug/96a4da43-407c-4c25-99a3-b60e9492c5f0)
+## Request
+
+#### **Method**: PUT
+>- Host: https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com
+>- Path: /mug/96a4da43-407c-4c25-99a3-b60e9492c5f0
+#### **Query Parameter(s)**
+
+
+
+
+**Description**:
+**Test URL**: [https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug/](https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug/)
+## Request
+
+#### **Method**: GET
+>- Host: https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com
+>- Path: /mug/
+#### **Query Parameter(s)**
+
+
+
+
+**Description**: Get a _single_ mug
+**Test URL**: [https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug/{id}](https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug/{id})
+## Request
+
+#### **Method**: GET
+>- Host: https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com
+>- Path: /mug/{id}
+#### **Query Parameter(s)**
+
+
+
+---
+### **Status Code**: 200 (OK)
+#### GET /mug/{id} - Sample Mug 1
+**Test URL**: [https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug/52fec04a-a6af-4849-a85d-ccf481ff73a0](https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com/mug/52fec04a-a6af-4849-a85d-ccf481ff73a0)
+#### **Method**: GET
+
+>- Host: https://ghpr8nzqbd.execute-api.us-west-2.amazonaws.com
+>- Path: /mug/52fec04a-a6af-4849-a85d-ccf481ff73a0
+#### **Query Parameter(s)**
+
+
+>- Key: Content-Type
+>- Value: text/plain; charset=utf-8
+
+#### **Body**
+```plain
+[{"capacity":240,"id":"52fec04a-a6af-4849-a85d-ccf481ff73a0","color":["Slate Grey","Blorange","blellow"],"droppable":true}]
+```
--- sample/test.js
+++ sample/test.js
@@ -1,84 +1,191 @@
 'use strict';
 
-let filepath = './assets/Pokemon Test.postman_collection.json';
+let filepath = './assets/Another.postman_collection.json';
 
-let input = require(filepath);
+let inputJSON = require(filepath);
 
-let thisItem = input.item;
-let allRoutes = "";
-for (let routeIdx = 0; routeIdx < thisItem.length; routeIdx++) {
-    const routeName = `### ${thisItem[routeIdx].name}`;
-    const routeDesc = `**Description**: ${thisItem[routeIdx].request.description}`;
-   
-    const routeTemplate = `
-    ${routeName}
+const authTpl = (requestAuthObj) => {
 
-    ${routeDesc}
-    
-    `;
+  if(!requestAuthObj){
+    return;
+  }
+  
+  let authTemplate = "";
+  let authTypeStr = requestAuthObj.type;
+  authTemplate += `#### **Authorization Type**: ${authTypeStr}
+`;
+  let authTypeArr = requestAuthObj[authTypeStr];
+  for(let i = 0; i < authTypeArr.length; i++) {
+    const authKeyStr = `>- Key: ${authTypeArr[i].key}`;
+    const authValueStr = `>- Value: ${authTypeArr[i].value}`;   
+    const authTypeTypeStr = `>- Type: ${authTypeArr[i].type}`;
+    authTemplate += 
+`${authKeyStr}
+${authValueStr}
+${authTypeTypeStr}
+`;
+  }
+  return authTemplate;
+};
 
-    allRoutes += routeTemplate;
-}
+const urlHostNameStr = (urlHostArray) => { // Expecting thisItem.request.url.host OR thisResponse.originalRequest.url.host
+  let hostName = "";
+  for (let i = 0; i < urlHostArray.length; i++) {
+    if (i === urlHostArray.length - 1) {
+      hostName += `${urlHostArray[i]}`
+    } else {
+      hostName += `${urlHostArray[i]}.`
+    }
+  }
+  return hostName; // Just a string representing the url host
+};
 
+const urlHostPathStr = (urlPathArray) => { // Expecting thisItem.request.url.path OR thisResponse.originalRequest.url.path
+  let hostPath = "";
+  for (let i = 0; i < urlPathArray.length; i++) {
+    hostPath += `/${urlPathArray[i]}`
+  }
+  return hostPath; // Just a string representing the url path
+};
 
-console.log(thisItem.request.auth.type);
-let allAuths = "";
-let authType = thisItem[0].request.auth.type;
+const urlQueryParamsTpl = (urlQueryArray) => { // Expecting thisItem.request.url.query OR thisResponse.originalRequest.url.query
+  if(!urlQueryArray){
+    return;
+  }
+  let hostParams = "";
+  for (let i = 0; i < urlQueryArray.length; i++) {
+    hostParams +=
+`##### Parameter (${urlQueryArray[i].description})
+>- key: ${urlQueryArray[i].key}  
+>- value: ${urlQueryArray[i].value}  
+`;
+  }
 
-for(let authIndx = 0; authIndx < thisItem.request.auth.type[authType].length; authIndx++) {
-    const authKey = `>- Key: ${thisItem.request.auth.type[authType].key}`;
-    const authValue = `>- Value: ${thisItem.request.auth.type[authType].value}`;   
-    const authorizationType = `>- Type: ${thisItem.request.auth.type[authType].type}`;
-    
-    const authTemplate = `
-    ${authKey}
+  return hostParams; // A template for all query parameters 
+};
 
-    ${authValue}
+const urlTpl = (urlObj) => { // Expecting thisItem.request.url OR thisResponse.originalRequest.url
+  // console.log(urlObj)
 
-    ${authorizationType}
-    `;
+  var queryString = '';
+  if (urlObj.query) {
+    queryString = urlQueryParamsTpl(urlObj.query);
+  }
 
-    allAuths += authTemplate;
-}
+  let urlTemplate =
+`>- Host: ${urlObj.protocol}://${urlHostNameStr(urlObj.host)}  
+>- Path: ${urlHostPathStr(urlObj.path)}  
+#### **Query Parameter(s)**
+${queryString}
+`;
 
+  return urlTemplate;
+};
 
-thisResponse = thisItem.response;
-let responseHeaders = thisResponse.header;
-let contentHeader = "";
-for(let headerIndx = 0; headerIndx < responseHeaders.length; headerIndx++) {
-    if(responseHeaders[headerIndx].key === "Content-Type") {
-        const headerKey = `>- Key: ${responseHeaders[headerIndx].key}`;
-        const headerValue = `>- Value: ${responseHeaders[headerIndx].value}`;
+const requestTpl = (requestObj) => { // Expecting thisItem.request OR thisResponse.originalRequest
+  // console.log('++++++++++', requestObj);
+  if(!requestObj.description){
+    requestObj.description = "";
+  }
+
+  var authString = "";
+  if (requestObj.auth) {
+    authString = authTpl(requestObj.auth);
+  }
+
+  let requestTemplate =
+`**Description**: ${requestObj.description}
+**Test URL**: [${requestObj.url.raw}](${requestObj.url.raw})
+## Request
+${authString}
+#### **Method**: ${requestObj.method}
+${urlTpl(requestObj.url)}
+`;
+
+  return requestTemplate;
+};
+
+const headerTpl = (headerArr) => { //We are expecting input.item[0].response[0].header
+  let headerTemplate = "";
+  
+  if (headerArr === null) {
+    return headerTemplate;
+  }
+  
+  for(let i = 0; i < headerArr.length; i++){
+    if(headerArr[i].key === "Content-Type") {
+      const headerKeyStr = `>- Key: ${headerArr[i].key}`;
+      const headerValueStr = `>- Value: ${headerArr[i].value}`;
+      headerTemplate += 
+`${headerKeyStr}
+${headerValueStr}
+`;
+    } 
+  }
+  return headerTemplate;
+};
+
+const responseTpl = (responseArr) => { // Expecting thisItem.response 
+  let responseTemplate = "";
+  for(let i = 0; i < responseArr.length; i++) {
+
+    var statusString = '';
+    if (responseArr[i].status) {
+      statusString = responseArr[i].status;
     }
 
-    const headerTemplate = `
-    ${headerKey}
-
-    ${headerValue}
-    `;
-
-    contentHeader += headerTemplate;
-}
-    
-
-
-
-
-
-
-let output = `
-## ${input.info.name}
-
-**Description**: ${input.info.description}
-
-[Postman Collection JSON](<${filepath}>)
-
-## Route(s)
-
-${allRoutes}
-${allAuths}
-${contentHeader}
-
-console.log(output);
-
+    // console.log("Response URL: " + responseArr[i].originalRequest.url)
+    responseTemplate +=
+`---
+### **Status Code**: ${responseArr[i].code} (${statusString})
+#### ${responseArr[i].name}
+**Test URL**: [${responseArr[i].originalRequest.url.raw}](${responseArr[i].originalRequest.url.raw})
+#### **Method**: ${responseArr[i].originalRequest.method}
+  
+${urlTpl(responseArr[i].originalRequest.url)}
+${headerTpl(responseArr[i].header)}
+#### **Body**
+\`\`\`${responseArr[i]._postman_previewlanguage}
+${responseArr[i].body}
+\`\`\`
 `;
+  }
+
+  return responseTemplate;
+};
+
+const routeTpl = (routeArr) => {
+  let routeTemplate = "";
+  // console.log(routeArr);
+  routeTemplate += `## Route(s)
+`;
+  // console.log(routeArr);
+  // We need to iterate over every route and find 
+  for(let i = 0; i < routeArr.length; i++) {
+    routeTemplate += `${requestTpl(routeArr[i].request)}
+`;
+    let responseArr = routeArr[i].response;
+    if(responseArr) {
+      routeTemplate +=
+`${responseTpl(responseArr)}
+`
+    }
+  }
+
+  return routeTemplate;
+};
+
+var descriptionString = "";
+if(inputJSON.info.description) {
+  descriptionString = inputJSON.info.description;
+}
+
+// console.log("Item: " + input.item);
+let output = 
+`## ${inputJSON.info.name}
+**Description**: ${descriptionString}
+[Postman Collection JSON](<${filepath}>)
+${routeTpl(inputJSON.item)}
+`;
+
+console.log("Output: " + output);
